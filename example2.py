d34c86774e753c301af05adff2b0750c1e35c4af
Manank Patni <manank321@gmail.com>
2017-07-13 21:29:19 +0530

    Update example2.py

--- example2.py
+++ example2.py
@@ -15,7 +15,7 @@ from unfollow_protocol import unfollow_protocol
 bot = InstaBot(
     login="fnapsinc",
     password="mananksakshambloginsta",
-    like_per_day=850,
+    like_per_day=24,
     comments_per_day=0,
     tag_list=['follow4follow', 'f4f', 'cute'],
     tag_blacklist=['rain', 'thunderstorm'],
